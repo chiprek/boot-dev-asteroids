840fbf60e921a85da01e16953233f86c7c8537e4
chiprek <chiprek.cs@gmail.com>
2025-08-17 22:51:26 -0500

    B: added clock

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock= pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +20,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        #frame rate limiter
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
